570a2ddcbdefbbe4ed1371b33c730042fa4e49a4
Bruno Horn <eusoubrunohorn@gmail.com>
2020-07-15 20:30:04 -0300

    comeco da implementacao tabuleiro

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,15 +1,13 @@
 package aplicacao;
 
-import camadaTabuleiro.Posicao; 
+import camadaTabuleiro.Posicao;
+import camadaTabuleiro.Tabuleiro; 
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Posicao pos = new Posicao(3, 4);
-		
-		
-		System.out.println(pos);
+	Tabuleiro tab = new Tabuleiro(8, 8);
 		
 		
 
