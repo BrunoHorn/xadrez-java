811e7ae5e5dede6833374047b5c265a52252273a
Bruno Horn <eusoubrunohorn@gmail.com>
2020-07-29 14:59:14 -0300

    limpar tabuleiro

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,7 +1,9 @@
 package aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import xadrez.ExcessaoXadez;
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
@@ -13,23 +15,42 @@ public class Programa {
 		PartidaXadrez partidaXadrez = new PartidaXadrez();
 		
 		while(true) {
-		UI.imprimirTabuleiro(partidaXadrez.getPecas());
-		System.out.println();
-		System.out.print("Inicial: ");
-		PosicaoXadrez inicial = UI.escrevendoPosicaoXadrez(sc);
-		
-		System.out.println();
-		System.out.print("posição final: ");
-		PosicaoXadrez finaal =UI.escrevendoPosicaoXadrez(sc);
-		
-		PecaXadrez capPecaXadrez = partidaXadrez.movimentarPecaXadrez(inicial, finaal);
+			try {
+			
+			UI.limparTela();
+			UI.imprimirTabuleiro(partidaXadrez.getPecas());
+			System.out.println();
+			System.out.print("Inicial: ");
+			PosicaoXadrez inicial = UI.escrevendoPosicaoXadrez(sc);
+			
+			System.out.println();
+			System.out.print("posição final: ");
+			PosicaoXadrez finaal =UI.escrevendoPosicaoXadrez(sc);
+			
+			PecaXadrez capPecaXadrez = partidaXadrez.movimentarPecaXadrez(inicial, finaal);
+			}
+			catch (ExcessaoXadez e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				}
 		
 		}
+		
+	}
+}	//catch(ExcessaoXadez}
+		
+			
+		
+		
 		
 	
 		
 		
 
-	}
+	
 
-}
+
